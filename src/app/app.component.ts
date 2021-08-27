7ef3a871dd8008b977f81e280a0b189475eea5b7
Ty Ponzo <52117423+tponzo@users.noreply.github.com>
2021-08-27 17:53:08 -0500

    aos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'tponzo-github-io';
+
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
